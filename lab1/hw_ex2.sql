586772da436ce7ac5b3b4348d5c880b5531ef5d3
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-10-19 16:09:46 +0200

    add lab2

--- lab1/hw_ex2.sql
+++ lab1/hw_ex2.sql
@@ -5,5 +5,7 @@ SELECT [member_no], [isbn], [fine_assessed], 2 * [fine_assessed] AS 'double fine
 SELECT [firstname] + ' ' + [middleinitial] + ' ' + [lastname] AS 'email_name' FROM [member] WHERE [lastname] = 'Anderson'
 
 SELECT LOWER([firstname] + [middleinitial] + SUBSTRING([lastname], 1, 2)) FROM [member]
-
+-- two version
 SELECT 'The title is: ' + [title] + ', title number ' + CAST([title_no] AS char) FROM [title]
+
+SELECT 'The title is: ' + [title] + ', title number ' + CONVERT(char, [title_no]) FROM [title]
