9cf9a75d7dbed7a8e4deeb7ef712ef145543314e
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-10-29 23:26:10 +0200

    add lab2

--- lab2/ex3.sql
+++ lab2/ex3.sql
@@ -10,3 +10,9 @@ SELECT TOP 1 DATENAME([WEEKDAY], [OrderDate]), COUNT([OrderID]) FROM [Orders]
 GROUP BY DATENAME([WEEKDAY], [OrderDate]) ORDER BY 2
 
 SELECT AVG(DATEDIFF(DAY, [OrderDate], [ShippedDate])) AS AVERAGE, MIN(DATEDIFF(DAY, [OrderDate], [ShippedDate])) AS MINI, MAX(DATEDIFF(DAY, [OrderDate], [ShippedDate])) AS MAXI FROM [Orders] WHERE [ShippedDate] <= [RequiredDate]
+
+SELECT [productid], [orderid], SUM([quantity]) AS [total_quantity] FROM [orderhist]
+GROUP BY [productid], [orderid] with cube
+
+SELECT [productid], [orderid], SUM([quantity]) AS [total_quantity] FROM [orderhist]
+GROUP BY [productid], [orderid] with rollup
