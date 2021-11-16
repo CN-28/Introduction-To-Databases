8288408bbe34611b0571bcc7ff2230df312d745b
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-11-16 15:16:19 +0100

    update lab3

--- lab3/hw_ex3.sql
+++ lab3/hw_ex3.sql
@@ -11,30 +11,30 @@ GROUP BY [CompanyName] ORDER BY [CNT] DESC
 SELECT [FirstName], [LastName], SUM([Quantity] * [UnitPrice] * (1 - [Discount])) FROM [Employees]
 INNER JOIN [Orders] ON [Orders].[EmployeeID] = [Employees].[EmployeeID]
 INNER JOIN [Order Details] ON [Order Details].[OrderID] = [Orders].[OrderID]
-GROUP BY [FirstName], [LastName]
+GROUP BY [Employees].[EmployeeID], [FirstName], [LastName]
 
 SELECT TOP 1 [FirstName], [LastName], COUNT(*) AS [CNT] FROM [Employees]
 INNER JOIN [Orders] ON [Orders].[EmployeeID] = [Employees].[EmployeeID]
 INNER JOIN [Order Details] ON [Order Details].[OrderID] = [Orders].[OrderID]
 WHERE YEAR([ShippedDate]) = 1997
-GROUP BY [FirstName], [LastName] ORDER BY [CNT] DESC
+GROUP BY [Employees].[EmployeeID], [FirstName], [LastName] ORDER BY [CNT] DESC
 
 SELECT TOP 1 [FirstName], [LastName], SUM([Quantity] * [UnitPrice] * (1 - [Discount])) AS [SUM] FROM [Employees]
 INNER JOIN [Orders] ON [Orders].[EmployeeID] = [Employees].[EmployeeID]
 INNER JOIN [Order Details] ON [Order Details].[OrderID] = [Orders].[OrderID]
 WHERE YEAR([ShippedDate]) = 1997
-GROUP BY [FirstName], [LastName] ORDER BY [SUM] DESC
+GROUP BY [Employees].[EmployeeID], [FirstName], [LastName] ORDER BY [SUM] DESC
 
-SELECT [E].[FirstName], [E].[LastName], SUM([Quantity] * [UnitPrice] * (1 - [Discount])) FROM [Employees] AS [E]
+SELECT DISTINCT [E].[FirstName], [E].[LastName], SUM([Quantity] * [UnitPrice] * (1 - [Discount])) FROM [Employees] AS [E]
 INNER JOIN [Orders] ON [Orders].[EmployeeID] = [E].[EmployeeID]
 INNER JOIN [Order Details] ON [Order Details].[OrderID] = [Orders].[OrderID]
 LEFT OUTER JOIN [Employees] AS [SUB] ON [E].[EmployeeID] = [SUB].[ReportsTo]
 WHERE [SUB].[EmployeeID] IS NOT NULL
-GROUP BY [E].[FirstName], [E].[LastName]
+GROUP BY [E].[EmployeeID], [SUB].[EmployeeID], [E].[FirstName], [E].[LastName]
 
 SELECT [E].[FirstName], [E].[LastName], SUM([Quantity] * [UnitPrice] * (1 - [Discount])) FROM [Employees] AS [E]
 INNER JOIN [Orders] ON [Orders].[EmployeeID] = [E].[EmployeeID]
 INNER JOIN [Order Details] ON [Order Details].[OrderID] = [Orders].[OrderID]
 LEFT OUTER JOIN [Employees] AS [SUB] ON [E].[EmployeeID] = [SUB].[ReportsTo]
 WHERE [SUB].[EmployeeID] IS NULL
-GROUP BY [E].[FirstName], [E].[LastName]
+GROUP BY [E].[EmployeeID], [SUB].[EmployeeID], [E].[FirstName], [E].[LastName]
