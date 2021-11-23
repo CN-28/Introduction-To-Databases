8df96b2676e5a7c92947180f214bd1c87ef7ca86
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-11-23 01:57:26 +0100

    add lab4 - subqueries

--- lab4/ex1.sql
+++ lab4/ex1.sql
@@ -4,3 +4,17 @@ WHERE [CustomerID] IN (SELECT [CustomerID] FROM [Orders]
                        AND YEAR([ShippedDate]) = 1997)
         
 
+SELECT [Companyname], [Phone] FROM [Customers]
+WHERE [CustomerID] IN (SELECT [CustomerID] FROM [Orders]
+                      WHERE [OrderID] IN (SELECT [OrderID] FROM [Order Details]
+                                         WHERE [ProductID] IN (SELECT [ProductID] FROM [Products]
+                                                              WHERE [CategoryID] IN (SELECT [CategoryID] FROM [Categories]
+                                                                                    WHERE [CategoryName] = 'Confections'))))
+
+
+SELECT [Companyname], [Phone] FROM [Customers]
+WHERE [CustomerID] NOT IN (SELECT [CustomerID] FROM [Orders]
+                      WHERE [OrderID] IN (SELECT [OrderID] FROM [Order Details]
+                                         WHERE [ProductID] IN (SELECT [ProductID] FROM [Products]
+                                                              WHERE [CategoryID] IN (SELECT [CategoryID] FROM [Categories]
+                                                                                    WHERE [CategoryName] = 'Confections'))))
